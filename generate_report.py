14257eb2417cbc1f91a84d05322430a6e83002ea
fateme-rwth <fateme.tardasti@rwth-aachen.de>
2021-07-09 18:05:56 +0200

    Fix write summary

--- generate_report.py
+++ generate_report.py
@@ -1,13 +1,13 @@
 def generate_report(encoder, decoder, vae, batch_size, kernel_size, filters, latent_dim, epochs, use_mse, load_weights, nb_layers, cwd):
     
     with open(cwd + '/encoder_summary.txt', 'w') as f:
-        f.write(encoder.summary())
+        encoder.summary(print_fn=lambda x: f.write(x + '\n'))
 
     with open(cwd + '/decoder_summary.txt', 'w') as f:
-        f.write(decoder.summary())
+        decoder.summary(print_fn=lambda x: f.write(x + '\n'))
 
     with open(cwd + '/vae_summary.txt', 'w') as f:
-        f.write(vae.summary())
+        vae.summary(print_fn=lambda x: f.write(x + '\n'))
 
     with open(cwd + '/parameters.txt', 'w') as f:
         txt = """
